08de9effafc2bcb92a6744734468cbd57be4bd3e
gabrielleite1797 <gabrielleite1797@gmail.com>
2023-09-25 21:23:58 -0300

    testa lacos, testa condicoes

--- src/main/kotlin/br/com/alura/leitebank/teste/testaLacos.kt
+++ src/main/kotlin/br/com/alura/leitebank/teste/testaLacos.kt
@@ -1,3 +1,5 @@
+package br.com.alura.leitebank.teste
+
 fun testaLacos() {
     var i = 0
     while (i < 5) {
--- src/main/kotlin/testaCondicoes.kt
+++
@@ -1,7 +0,0 @@
-fun testaCondicoes(saldo: Double) {
-    when {
-        saldo > 0.0 -> println("Conta positiva")
-        saldo == 0.0 -> println("Conta é Neutra")
-        else -> println("Conta Negativa")
-    }
-}
